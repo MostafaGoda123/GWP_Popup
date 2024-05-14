556231c79f8259fb9d4cfa73b2bf632d36196666
Eng\ Mostafa El-Sayed <104257870+MostafaGoda123@users.noreply.github.com>
2024-05-14 15:01:34 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+document.getElementById("btnShow").onclick = function () {
+   document.querySelector(".up_down").classList.remove("hide")
+}
+document.getElementById("btnHide").onclick = function () {
+   document.querySelector(".up_down").classList.add("hide")
+}
+
